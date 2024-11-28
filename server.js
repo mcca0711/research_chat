9d646225f7240da8ff7a26f8846616988ae2bde3
Matthew J. M <orbit9112@gmail.com>
2024-11-27 23:11:52 -0500

    Added vercel.json

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const chatRouter = require('./api/chat');
 const serverless = require('serverless-http');
 
 const app = express();
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/chat', chatRouter);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 // Export the app as a serverless function
 module.exports.handler = serverless(app);
